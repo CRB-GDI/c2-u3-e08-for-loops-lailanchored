a28d51557dd986c0d6d667c9f8ba4ad6ef4459b6
Laila D <laila.daniels@s.capregboces.org>
2024-02-28 10:19:29 -0500

    Through Step 2

---
+++ Excercise8/Excercise8/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Excercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is the maximum number : ");
+            int maxNum = int.Parse(Console.ReadLine());
+       
+
+            int evenNums = 2;
+
+            for (int i = 0; i < maxNum; i++)
+            { 
+            Console.Write("Even Numbers of" + maxNum + "are" + i - evenNums);
+
+        }
+            
+        }
+    }
+}
