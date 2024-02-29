a8c5bdd46ccf6b68d65d03edd648e6df0c47be3c
Laila D <laila.daniels@s.capregboces.org>
2024-02-29 09:43:22 -0500

    Ready For Grading

--- Excercise8/Excercise8/Program.cs
+++ Excercise8/Excercise8/Program.cs
@@ -9,13 +9,42 @@
             int maxNum = int.Parse(Console.ReadLine());
        
 
-            int evenNums = 2;
+            int evenNums = 0;
 
-            for (int i = 0; i < maxNum; i++)
-            { 
-            Console.Write("Even Numbers of" + maxNum + "are" + i - evenNums);
+            for (evenNums = 0; evenNums <= maxNum;evenNums += 2)
 
-        }
+            {
+
+                Console.WriteLine(" Even Numbers of " + maxNum + " are " + evenNums);
+
+
+               
+
+            }
+            //end ur loop
+
+            //start prob 2
+        {
+
+                Console.Write("What Row Ridth");
+                int rowWidth = int.Parse(Console.ReadLine());
+                int asteriskCount = 0;
+
+                for (asteriskCount = 0; rowWidth > asteriskCount; asteriskCount++)
+                {
+
+                    Console.Write("*");
+                    // int.Parse(Console.WriteLine("*" * rowWidth));
+
+
+
+
+
+
+
+                }
+
+            }
             
         }
     }
